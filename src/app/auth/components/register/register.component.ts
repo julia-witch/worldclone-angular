491ff3cfaf137d29ebdd8968a0d5b3f5df7ebe00
Your Name <julia1603@gmail.com>
2021-01-10 13:19:58 +0300

    initialazeForm

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  form: FormGroup
+  constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
+    this.initializeForm()
   }
 
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: '',
+      password: '',
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value)
+  }
 }
