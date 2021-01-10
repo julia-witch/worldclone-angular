1eafe4fa963abe74c20fef32674dc816d89d4543
Юлия Гулла <julia_gulla@Mac-mini.lan>
2021-01-10 12:08:37 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+
+import {AppRoutingModule} from 'src/app/app-routing.module'
+import {AppComponent} from 'src/app/app.component'
+import {AuthModule} from './auth/auth.module'
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
